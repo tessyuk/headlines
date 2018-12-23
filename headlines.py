3876f9403f3badd7d1a0c9d97e3cddc162e9ad7f
Tessy Badriyah <soton.pompey@gmail.com>
2018-12-23 13:45:22 +0700

    after changes

--- headlines.py
+++ headlines.py
@@ -8,7 +8,7 @@ def get_news():
     first_article = feed['entries'][0]  
     return """<html>    
 <body>        
- <h1> BBC Headlines </h1>
+ <h1> BBC Headlines ...this one is a new one on a new branch git</h1>
  <b>{0}</b> <br/> 
  <i>{1}</i> <br/>
  <p>{2}</p> <br/>    
