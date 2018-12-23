c08c597819fc3134d1c04ff414e3bfc81083741a
Tessy Badriyah <soton.pompey@gmail.com>
2018-12-23 16:46:39 +0700

    second last change

--- headlines.py
+++ headlines.py
@@ -1,14 +1,31 @@
 import feedparser
 from flask import Flask
 app = Flask(__name__)
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+'cnn': 'http://rss.cnn.com/rss/edition.rss',
+'fox': 'http://feeds.foxnews.com/foxnews/latest',
+'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/") 
-def get_news():  
-    feed = feedparser.parse(BBC_FEED)  
-    first_article = feed['entries'][0]  
-    return """<html>    
+@app.route("/bbc")
+
+def bbc():
+     return get_news('bbc')
+@app.route("/cnn")
+def cnn():
+     return get_news('cnn')
+@app.route("/fox")
+def fox():
+     return get_news('fox')
+@app.route("/iol")
+def iol():
+     return get_news('iol')
+def get_news(publication):
+     feed = feedparser.parse(RSS_FEEDS[publication])
+     first_article = feed['entries'][0]
+     return """<html>
 <body>        
- <h1> BBC Headlines</h1>
+ <h1>Headline</h1> 	 
  <b>{0}</b> <br/> 
  <i>{1}</i> <br/>
  <p>{2}</p> <br/>    
